e53345a76ee0e2c815cf9a93971ca8addad745d7
kwong-charles <63695684+kwong-charles@users.noreply.github.com>
2020-04-30 00:28:58 +0800

    Add files via upload

--- bt10.c
+++ bt10.c
@@ -4,14 +4,13 @@
 int main() {
     FILE *fp;
     fp = fopen("C:\\C_Projects\\numbers.txt", "r");
-    if ((fp = fopen("C:\\C_Projects\\numbers.txt","r")) == NULL){
-       printf("Error! opening file");
-    }
-    int max, min, no;
+    int max, min, no, len;
     float sum;
     sum = 0;
     max = 0;
+    len = 0;
     min = 10000;
+    int arr[2][10000];
     for(int i = 0 ; i <10000 ; i++){
         fscanf(fp, "%d", &no);
         sum += no;
